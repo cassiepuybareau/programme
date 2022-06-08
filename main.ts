483e32c210037d37e6a304cb27a61ba830864af0
cassiepuybareau <103409994+cassiepuybareau@users.noreply.github.com>
2022-06-08 10:23:44 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(2)
+})
+basic.showIcon(IconNames.Angry)
+radio.setGroup(8)
+basic.forever(function () {
+	
+})
